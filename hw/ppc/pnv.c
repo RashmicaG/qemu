e3a9eba71c63f7df6400dcbdf23cfe1fc47dacf5
Benjamin Herrenschmidt <benh@kernel.crashing.org>
2019-09-18 18:02:03 +0200

    ppc/pnv: Add model for POWER9 PHB4 PCIe Host bridge
    The links betweend the PHB4 and PEC objects needs some rework.
    
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Signed-off-by: Cédric Le Goater <clg@kaod.org>

--- hw/ppc/pnv.c
+++ hw/ppc/pnv.c
@@ -1101,7 +1101,10 @@ static void pnv_chip_power8nvl_class_init(ObjectClass *klass, void *data)
 
 static void pnv_chip_power9_instance_init(Object *obj)
 {
+    PnvChip *chip = PNV_CHIP(obj);
     Pnv9Chip *chip9 = PNV9_CHIP(obj);
+    PnvChipClass *pcc = PNV_CHIP_GET_CLASS(obj);
+    int i;
 
     object_initialize_child(obj, "xive", &chip9->xive, sizeof(chip9->xive),
                             TYPE_PNV_XIVE, &error_abort, NULL);
@@ -1127,6 +1130,17 @@ static void pnv_chip_power9_instance_init(Object *obj)
                             TYPE_PNV9_HOMER, &error_abort, NULL);
     object_property_add_const_link(OBJECT(&chip9->homer), "chip", obj,
                                    &error_abort);
+
+    for (i = 0; i < PNV9_CHIP_MAX_PEC; i++) {
+        object_initialize_child(obj, "pec[*]", &chip9->pecs[i],
+                                sizeof(chip9->pecs[i]), TYPE_PNV_PHB4_PEC,
+                                &error_abort, NULL);
+    }
+
+    /*
+     * Number of PHBs is the chip default
+     */
+    chip->num_phbs = pcc->num_phbs;
 }
 
 static void pnv_chip_quad_realize(Pnv9Chip *chip9, Error **errp)
@@ -1164,6 +1178,7 @@ static void pnv_chip_power9_realize(DeviceState *dev, Error **errp)
     PnvChip *chip = PNV_CHIP(dev);
     Pnv9Psi *psi9 = &chip9->psi;
     Error *local_err = NULL;
+    uint32_t i;
 
     /* XSCOM bridge is first */
     pnv_xscom_realize(chip, PNV9_XSCOM_SIZE, &local_err);
@@ -1247,6 +1262,31 @@ static void pnv_chip_power9_realize(DeviceState *dev, Error **errp)
     }
     memory_region_add_subregion(get_system_memory(), PNV9_HOMER_BASE(chip),
                                 &chip9->homer.regs);
+
+    /* PHB4 PECs */
+    for (i = 0; i < PNV9_CHIP_MAX_PEC && i < chip->num_phbs; i++) {
+        PnvPhb4PecState *pec = &chip9->pecs[i];
+
+        object_property_set_int(OBJECT(pec), i, "index", &error_fatal);
+        object_property_set_int(OBJECT(pec), chip->chip_id, "chip-id", &error_fatal);
+        object_property_set_bool(OBJECT(pec), true, "realized", &local_err);
+        if (local_err) {
+            error_propagate(errp, local_err);
+            return;
+        }
+    }
+
+    /* PHB4 controllers */
+    for (i = 0; i < chip->num_phbs; i++) {
+        Object *obj = object_new(TYPE_PNV_PHB4);
+        object_property_set_int(obj, chip->chip_id, "chip-id", &error_fatal);
+        object_property_set_int(obj, i, "index", &error_fatal);
+        object_property_set_bool(obj, true, "realized", &local_err);
+        if (local_err) {
+            error_propagate(errp, local_err);
+            return;
+        }
+    }
 }
 
 static void pnv_chip_power9_class_init(ObjectClass *klass, void *data)
@@ -1257,7 +1297,7 @@ static void pnv_chip_power9_class_init(ObjectClass *klass, void *data)
     k->chip_type = PNV_CHIP_POWER9;
     k->chip_cfam_id = 0x220d104900008000ull; /* P9 Nimbus DD2.0 */
     k->cores_mask = POWER9_CORE_MASK;
-    k->num_phbs = 0;
+    k->num_phbs = 6;
     k->core_pir = pnv_chip_core_pir_p9;
     k->intc_create = pnv_chip_power9_intc_create;
     k->isa_create = pnv_chip_power9_isa_create;
